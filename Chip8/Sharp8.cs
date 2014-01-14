5aa6b7b19ab97cbdf500a469caf2b63063ac0f69
Matt L <donoteatmikezila@gmail.com>
2014-01-14 17:58:38 -0500

    more gui work
    changes from GTK to WinForms

--- Chip8/Sharp8.cs
+++ Chip8/Sharp8.cs
@@ -7,28 +7,8 @@ namespace Sharp8
 	class MainClass : Form
 	{
 		private static CHIP8CPU cpu;
-
-		public MainClass ()
-		{
-			Text = "Sharp8";
-			Size = new Size (320, 240);
-			FormBorderStyle = FormBorderStyle.FixedSingle;
-			MaximizeBox = false;
-			MinimizeBox = false;
-			CenterToScreen ();
-
-			Button step = new Button ();
-			step.Parent = this;
-			step.Text = "Step";
-			step.Location = new Point (10, 10);
-			step.Click += StepClicked;
-
-			Button ten_steps = new Button ();
-			ten_steps.Parent = this;
-			ten_steps.Text = "Step (10)";
-			ten_steps.Location = new Point (200, 10);
-			ten_steps.Click += TenStepsClicked;
-		}
+		private bool running = true;
+		private int sleep_time = 17;
 
 		public static void Main (string[] args)
 		{
@@ -37,33 +17,84 @@ namespace Sharp8
 
 		}
 
+		public MainClass ()
+		{
+			Text = "Sharp8";
+			Size = new Size (64 * 6, 480);
+			FormBorderStyle = FormBorderStyle.FixedSingle;
+			MaximizeBox = false;
+			MinimizeBox = false;
+			CenterToScreen ();
+
+			int buttonWidth = 100;
+
+			Button pause = new Button ();
+			pause.Parent = this;
+			pause.Text = "Pause";
+			pause.Width = buttonWidth;
+			pause.Location = new Point (10, 202);
+			pause.Click += PauseClicked;
+
+			Button step = new Button ();
+			step.Parent = this;
+			step.Text = "Step";
+			step.Width = buttonWidth;
+			step.Location = new Point (110, 202);
+			step.Click += StepClicked;
+
+			Button speed = new Button ();
+			speed.Parent = this;
+			speed.Text = "Speed";
+			speed.Width = buttonWidth;
+			speed.Location = new Point (210, 202);
+			speed.Click += SpeedToggle;
+		}
+
 		protected override void OnPaint (PaintEventArgs e)
 		{
 			base.OnPaint (e);
-			e.Graphics.DrawImage (cpu.raster, 50, 50, 64 * 4, 32 * 4);
+			e.Graphics.DrawImage (cpu.raster, 0, 0, 64 * 6, 32 * 6);
+			if (running) {
+				Emulate ();
+			}
+			this.Invalidate (true);
 		}
 
-		public void TenStepsClicked (object sender, EventArgs e)
+		public void PauseClicked (object sender, EventArgs e)
 		{
-			Emulate (10);
-			this.Invalidate (true);
+			Button button = (Button)sender;
+			if (running)
+				button.Text = "Run";
+			else
+				button.Text = "Pause";
+			running = !running;
 		}
 
 		public void StepClicked (object sender, EventArgs e)
 		{
-			Emulate ();
-			this.Invalidate (true);
+			if (!running)
+				Emulate ();
 		}
 
-		public static void Emulate (int cycles = 1)
+		void SpeedToggle (object sender, EventArgs e)
 		{
-			while (cycles > 0) {
-				cpu.RunCycle ();
-				cycles--;
-				if (cpu.crashed)
-					cycles = 0;
+			if (sleep_time == 17) {
+				sleep_time = 1;
+			} else {
+				sleep_time = 17;
 			}
+		}
+
+		public void Emulate ()
+		{
+			if (cpu.crashed) {
+				cpu.CrashDump ();
+				return;
+			}
+			cpu.RunCycle ();
 			cpu.UpdateScreen ();
+			System.Threading.Thread.Sleep (sleep_time);
+
 		}
 	}
 }
